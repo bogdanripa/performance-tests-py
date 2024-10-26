f73a1285e078c89dfbe10eb1a4792aaff653def4
bogdanripa <bogdanripa@gmail.com>
2024-10-26 14:38:13 +0300

    using urllib3

--- coldStartTest.py
+++ coldStartTest.py
@@ -1,4 +1,4 @@
-import requests
+import urllib3
 import os
 import time
 
@@ -6,12 +6,13 @@ def lambda_handler(event):
     print('START')
     start_time = time.time()
     
-    response = requests.get('https://d33b1b0c-a88c-44eb-9047-8f57a3fe1166.eu-central-1.cloud.genez.io')
+    http = urllib3.PoolManager()
+    response = http.request('GET', 'https://d33b1b0c-a88c-44eb-9047-8f57a3fe1166.eu-central-1.cloud.genez.io')
     #os.environ['GENEZIO_CHILD_URL'])
     fetch_time = int((time.time() - start_time) * 1000)  # milliseconds
 
-    if not response.ok:
-        raise Exception(f'Network response was not ok: {response.status_code} {response.reason}')
+    if response.status != 200:
+        raise Exception(f'Network response was not ok: {response.status}')
 
     print(f'DONE in {fetch_time}ms')
 
